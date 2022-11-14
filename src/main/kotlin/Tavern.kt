df7c2a7546f8dd394aac72c4eabd34e3ae337f98
George <prich845@gmail.com>
2022-11-14 06:26:24 +0300

    Этот код стал раббочим. Режим отладки творит чудеса. Но по учебкику - все равно не правильно.

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -1,11 +1,12 @@
 fun main() {
     var beverage = readLine()
 
-    if (beverage == null) {
+    if (beverage == null || beverage == "") {
         println("I can't do that without crashing - beverage was null!")
     } else {
         beverage = beverage.replaceFirstChar { it.titlecase() }
+        println(beverage)
     }
 
-    println(beverage)
+
 }
